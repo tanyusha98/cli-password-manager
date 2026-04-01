fc076244164294fb7a3b4b5f7bfd3d47cf6c6b45
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-04-01 12:50:55 +0300

    Add load function

--- manager/storage.py
+++ manager/storage.py
@@ -2,3 +2,9 @@ import json
 def save(data, filename="passwords.json"):
     with open(filename, "w") as f:
         json.dump(data, f)
+def load(filename="passwords.json"):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except:
+        return []
