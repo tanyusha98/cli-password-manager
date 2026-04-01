dae14dd57f12b585bc734c5c073a43a27222d0ca
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-04-01 12:54:58 +0300

    Improve password complexity

--- manager/generator.py
+++ manager/generator.py
@@ -3,3 +3,6 @@ import string
 def generate_password(length=8):
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits + "!@#"
+    return ''.join(random.choice(chars) for _ in range(length))
