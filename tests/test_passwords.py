2460f38324af2c36d19dbc915a7e3b278c63306b
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-04-01 12:53:37 +0300

    Add generator tests

--- tests/test_passwords.py
+++ tests/test_passwords.py
@@ -3,3 +3,8 @@ from manager.passwords import add_password, list_passwords
 def test_add_password():
     add_password("test", "user", "pass")
     assert len(list_passwords()) > 0
+from manager.generator import generate_password
+
+def test_generate():
+    pwd = generate_password()
+    assert len(pwd) == 8
